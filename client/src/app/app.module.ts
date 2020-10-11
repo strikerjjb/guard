8ac6e454de2fe045b18b638c62c0974ef0d2600e
Josh Bartels <jbartels@gmail.com>
2020-10-11 10:11:41 -0400

    additional components for error modal

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './modules/material/material.module'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './modules/login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from  '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { ErrorComponent } from '../error/error.component'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  public  username:  string  =  "";
+    public  password:  string  =  "";
 
-  ngOnInit(): void {
-  }
+
+    constructor(private  dialog:  MatDialog, private  router: Router) { }
+    login(){
+        if(this.username  ===  "demo"  &&  this.password  === "demo")
+        {
+            this.router.navigate(['customer-list']);
+        }
+        else
+        {
+            this.dialog.open(ErrorComponent,{ data: {
+            message:  "Your login information are incorrect!"
+            }});
+        }
+      }
 
 }
